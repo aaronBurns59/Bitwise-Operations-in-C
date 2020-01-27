f92966dde24e71bd701cc903ba79a107d3409500
aaronburns59 <aaronburns59@gmail.com>
2020-01-27 23:43:47 +0000

    Added executable to the gitignore file

--- lookAtBits.c
+++ lookAtBits.c
@@ -3,7 +3,7 @@
 
 // prints each of the bits in s, one at a time
 void printBits(uint64_t s){
-	for(int i = 63; i >= 0; i--)
+	for(int i = (8-1); i >= 0; i--)
 	// Terrnary operator
 	// Shifting i position 1 to the left
 	// "ULL" makes c treat the 1 as an unsigned long long type
