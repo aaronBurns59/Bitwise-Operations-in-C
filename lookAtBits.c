5567d9004d874ce05d0da27861dad731d7a3a680
aaronburns59 <aaronburns59@gmail.com>
2020-01-27 23:39:05 +0000

    Created a function that prints the bits of a 64 bit integer

--- lookAtBits.c
+++ lookAtBits.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <inttypes.h>
+
+// prints each of the bits in s, one at a time
+void printBits(uint64_t s){
+	for(int i = 63; i >= 0; i--)
+	// Terrnary operator
+	// Shifting i position 1 to the left
+	// "ULL" makes c treat the 1 as an unsigned long long type
+	// Literal values have types
+	printf("%d", ((1ULL << i) & s) ? 1 : 0);
+}
 
 int main(int argc, char*argv[]){
-
-  int i = 65;
-	char c = 65;
-	long int l = 65;
-	long long int m = 65;	
-
-	uint8_t u = 65;// Guarenteed 8 bit integer
-	uint64_t e = 65;// Guarenteed 64 bit integer
-
-	printf("%x\t%c\t%d\t%d\n", c, c, c, sizeof(c));
-	printf("%x\t%c\t%d\t%d\n", i, i, i, sizeof(i));
-	printf("%x\t%c\t%d\t%d\n", l, l, l, sizeof(l));	
-	printf("%x\t%c\t%d\t%d\n", m, m, m, sizeof(m));	
-
+	
+	uint64_t s = 65;// Guarenteed 64 bit integer
+	
+	printBits(s);
+	// ll informs printf of the type
+	printf("\t%llx\t%lld\n", s, s);
 	return 0;
 }
 
