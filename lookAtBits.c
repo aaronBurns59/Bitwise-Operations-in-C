d365aba182ba85d76d673d106ed24291ab09c833
aaronburns59 <aaronburns59@gmail.com>
2020-01-28 00:20:32 +0000

    Testing Logical operators and bit shifting

--- lookAtBits.c
+++ lookAtBits.c
@@ -2,7 +2,7 @@
 #include <inttypes.h>
 
 // prints each of the bits in s, one at a time
-void printBits(uint64_t s){
+void printBits(uint8_t s){
 	for(int i = (8-1); i >= 0; i--)
 	// Terrnary operator
 	// Shifting i position 1 to the left
@@ -12,12 +12,39 @@ void printBits(uint64_t s){
 }
 
 int main(int argc, char*argv[]){
+	// C should be made up of the bits that are matching bit 
+	// positions in A and B
+	int8_t a = 73;
+	int8_t b = 99;
+	int8_t c = (a & b);
+
+	printf("Logical AND, '&' operator\n");
+	printf("a = "); printBits(a); printf("\n");
+	printf("b = "); printBits(b); printf("\n");
+	printf("c = "); printBits(c); printf("\n");
+
+	c = (a | b);
+
+	printf("Logical OR, '|' operator\n");
+	printf("a = "); printBits(a); printf("\n");
+	printf("b = "); printBits(b); printf("\n");
+	printf("c = "); printBits(c); printf("\n");
+
+	c = (a ^ b);
+
+	printf("Logical XOR, '^' operator\n");
+	printf("a = "); printBits(a); printf("\n");
+	printf("b = "); printBits(b); printf("\n");
+	printf("c = "); printBits(c); printf("\n");
 	
-	uint64_t s = 65;// Guarenteed 64 bit integer
-	
-	printBits(s);
-	// ll informs printf of the type
-	printf("\t%llx\t%lld\n", s, s);
+	printf("Logical NOT, '~' operator\n");
+	printf("a = "); printBits(a); printf("\n");
+	printf("~a = "); printBits(~a); printf("\n");
+
+	printf("Bit Shifting Numbers\n");
+	printf("1      = "); printBits(1); printf("\n");
+	printf("1 << 3 = "); printBits((1 <<3)); printf("\n");
+
 	return 0;
 }
 
